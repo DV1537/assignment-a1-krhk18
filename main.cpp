3aee30018c7187795802f7e66a15f86ba55c45f7
krhk18 <44838091+krhk18@users.noreply.github.com>
2018-11-08 14:53:24 +0100

    Added sum, average and print if above average

--- main.cpp
+++ main.cpp
@@ -15,30 +15,43 @@ int main(int argc, const char * argv[])
     int size = 1;
     int *numbersPtr = new int[size];
     int count = 0;
+    int sum = 0;
+    double average = 0;
     std::ifstream inputFile;
 
     inputFile.open(argv[1]);
 
-    if(!inputFile)
+    if(!inputFile)      //Checks if read in successfully
         std::cout << "Error opening file." << std::endl;
     else
     {
-        while(inputFile >> numbersPtr[count])
+        while(inputFile >> numbersPtr[count])           //Read in and store
         {
             count++;
-            if(count >= size)
+            if(count >= size)       //If full: expand
             {
-                int *tempPtr = new int[count + 1];
-                for(int i = 0; i < count; i++)
+                int *tempPtr = new int[count + 1];  //Create new, bigger
+                for(int i = 0; i < count; i++)      //move
                     tempPtr[i] = numbersPtr[i];
-                delete []numbersPtr;
-                numbersPtr = tempPtr;
+                delete []numbersPtr;                //delete old content
+                numbersPtr = tempPtr;               //make pointer point to new array
             }
         }
 
+        //Sum elements in array
         for(int i = 0; i < count; i++)
         {
-            std::cout << numbersPtr[i] << std::endl;
+            sum += numbersPtr[i];
+        }
+
+        //Average
+        average = static_cast<double>(sum) / count;
+
+        //Print above average
+        for(int i = 0; i < count; i++)
+        {
+            if(numbersPtr[i] > average)
+                std::cout << numbersPtr[i] << " ";
         }
     }
     return 0;
