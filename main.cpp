4c09af83ac8ab4a2e89e8b9cb2d46946307b6323
krhk18 <44838091+krhk18@users.noreply.github.com>
2018-11-08 08:46:09 +0100

    Marked out Eriks code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <fstream>
 
-//Change
-
 /**
  * The program reads numbers from a file and prints the sum of them
  * Example: main.exe ./tests/input.in
@@ -10,30 +8,32 @@
  * @param  argv [description]
  * @return      [description]
  */
-int main(int argc, const char * argv[])
+int main(int argc, const char * argv[])     //argc = number of files, argv[] file sent to the program
 {
+    //Added by help from Andreas. Prints the path to the file sent to the program????
     for(int i = 0; i < argc; i++)
     {
         std::cout << argv[i] << std::endl;
     }
-    
+
+//Code to read in and sum the numbers in the file. - Entered by Eriks.
+/*
     int a = 0;
     int sum = 0;
     
     std::ifstream myReadFile;
     
     myReadFile.open(argv[1]);
-    
+
     while (myReadFile >> a)
     {
         sum += a;
     }
     myReadFile.close();
-    
-    
-    
+        
     std::cout << sum << "\n";
-    
+*/
+
     return 0;
 }
 
